80c18c11b0e7d34a4c9d6fc1d43e8aaa35c11f66
AgileWebDev92 <billpaxton92@gmail.com>
2024-08-06 00:04:24 +0900

    update for pagination

--- src/pages/collections.js
+++ src/pages/collections.js
@@ -1,18 +1,25 @@
 import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { toast } from 'react-toastify';
+import { Commet } from 'react-loading-indicators';
+import ReactPaginate from 'react-paginate';
 import {
   ALERT_ERROR_CONFIG,
   API_PATH,
   DOWNLOAD_PATH,
   SUCCESS,
 } from '../utils/constants';
-import axios from 'axios';
-import { toast } from 'react-toastify';
 import CollectionCard from '../components/collectioncard';
 
 const Collections = () => {
   const [collections, setCollections] = useState([]);
+  const [currentItems, setCurrentItems] = useState([]);
+  const [loadingVisibility, setLoadingVisibility] = useState(false);
+  const [itemOffset, setItemOffset] = useState(0);
+  const [pageCount, setPageCount] = useState(0);
   useEffect(() => {
     const fetchData = async () => {
+      console.log('fetchData');
       const res = await axios.get(`${API_PATH}/collections/getCollections`);
       console.log(res);
       if (res.data.status === SUCCESS) {
@@ -26,19 +33,31 @@ const Collections = () => {
           };
         });
         setCollections(items);
+        setCurrentItems(items.slice(itemOffset, 4));
+        setPageCount(Math.ceil(items.length / 4));
+        setLoadingVisibility(false);
       } else {
         toast.error(res.data.message, ALERT_ERROR_CONFIG);
       }
     };
+    setLoadingVisibility(true);
     fetchData();
   }, []);
 
-  const getCollection = (symbol) => {
-    window.location.href = `/collection/${symbol}`;
+  // Invoke when user click to request another page.
+  const handlePageClick = (event) => {
+    const newOffset = (event.selected * 4) % collections.length;
+    console.log(
+      `User requested page number ${event.selected}, which is offset ${newOffset}`
+    );
+    const endOffset = newOffset + 4;
+    const items = collections.slice(newOffset, endOffset);
+    setCurrentItems(items);
+    setItemOffset(newOffset);
   };
 
   return (
-    <div className='px-2 flex flex-col pt-20 lg:pt-48 text-white w-screen h-full'>
+    <div className='px-4 flex flex-col pt-24 lg:pt-48 text-white w-screen h-full'>
       <div className='flex justify-end pb-4'>
         <a
           className='bg-gold w-fit px-2 py-1 text-dark-text rounded-sm text-lg'
@@ -47,17 +66,41 @@ const Collections = () => {
           Create Collection
         </a>
       </div>
-      <div className='grid lg:grid-cols-4 grid-cols-2 lg:gap-16 gap-4 m-auto'>
-        {collections && collections.length > 0 ? (
-          collections.map((item, index) => {
-            return <CollectionCard collection={item} />;
-          })
-        ) : (
-          <div className='text-center col-span-2 lg:col-span-4 text-3xl'>
-            <span>No Data Found</span>
+      {loadingVisibility ? (
+        <div className='w-full h-full py-20 mx-auto flex justify-center'>
+          <Commet color='#ebad2c' size='medium' text='' textColor='' />
+        </div>
+      ) : (
+        <>
+          <div className='grid lg:grid-cols-4 grid-cols-2 lg:gap-16 gap-4 m-auto'>
+            {collections && collections.length > 0 ? (
+              currentItems.map((item, index) => {
+                return <CollectionCard key={index} collection={item} />;
+              })
+            ) : (
+              <div className='text-center col-span-2 lg:col-span-4 text-3xl'>
+                <span>No Data Found</span>
+              </div>
+            )}
           </div>
-        )}
-      </div>
+          {collections && collections.length > 0 && (
+            <div className='flex flex-row'>
+              <ReactPaginate
+                className='flex flex-row gap-6 pt-8 w-full justify-center'
+                breakLabel='...'
+                nextLabel='Next'
+                onPageChange={handlePageClick}
+                pageRangeDisplayed={4}
+                pageCount={pageCount}
+                previousLabel='Prev'
+                previousLinkClassName='bg-slate-400 rounded-full px-4 py-1'
+                nextLinkClassName='bg-slate-400 rounded-full px-4 py-1'
+                renderOnZeroPageCount={null}
+              />
+            </div>
+          )}
+        </>
+      )}
       {/* <table className='w-full'>
         <tr>
           <th className='w-4'>#</th>
